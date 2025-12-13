cb2af457ef4e0fe4af8223801105478a6baffa8b
Rafael Alberto <rafael.alberto1703@gmail.com>
2025-12-13 18:39:58 -0300

    Domain foundation ready

--- src/main/java/io/rafaalberto/transaction_stream_processor/domain/entity/Transaction.java
+++ src/main/java/io/rafaalberto/transaction_stream_processor/domain/entity/Transaction.java
@@ -12,7 +12,7 @@ public final class Transaction {
   private final Instant occurredAt;
 
   public Transaction(final TransactionID id, final BigDecimal amount, final Instant occurredAt) {
-    this.id = Objects.requireNonNull(id, "TransactionID cannot be null");
+    this.id = Objects.requireNonNull(id, "TransactionID value cannot be null");
     this.amount = Objects.requireNonNull(amount, "Amount cannot be null");
     this.occurredAt = Objects.requireNonNull(occurredAt, "Occurred at cannot be null");
     validate();
@@ -38,5 +38,18 @@ public final class Transaction {
     if (amount.compareTo(BigDecimal.ZERO) <= 0) {
       throw new InvalidTransactionException("Transaction amount must be positive");
     }
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Transaction that = (Transaction) o;
+    return Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
   }
 }
