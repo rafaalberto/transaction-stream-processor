ecde65b28d2d075daa44b73c75afbd4a4022f7fb
Rafael Alberto <rafael.alberto1703@gmail.com>
2025-12-19 09:20:45 -0300

    Created repository contract (#9)

--- src/main/java/io/rafaalberto/transactionstreamprocessor/application/usecases/CreateTransactionUseCase.java
+++ src/main/java/io/rafaalberto/transactionstreamprocessor/application/usecases/CreateTransactionUseCase.java
@@ -1,15 +1,24 @@
 package io.rafaalberto.transactionstreamprocessor.application.usecases;
 
+import io.rafaalberto.transactionstreamprocessor.application.repository.TransactionRepository;
 import io.rafaalberto.transactionstreamprocessor.domain.transaction.Money;
 import io.rafaalberto.transactionstreamprocessor.domain.transaction.Transaction;
 
 public final class CreateTransactionUseCase {
 
+  private final TransactionRepository transactionRepository;
+
+  public CreateTransactionUseCase(final TransactionRepository transactionRepository) {
+    this.transactionRepository = transactionRepository;
+  }
+
   public Transaction execute(final CreateTransactionCommand command) {
-    return Transaction.create(
-        new Money(command.amount(), command.currency()),
-        command.type(),
-        command.occurredAt(),
-        command.externalReference());
+    var transaction =
+        Transaction.create(
+            new Money(command.amount(), command.currency()),
+            command.type(),
+            command.occurredAt(),
+            command.externalReference());
+    return transactionRepository.save(transaction);
   }
 }
