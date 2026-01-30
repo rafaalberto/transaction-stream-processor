1b2787ca1fddb8fc227b2fb6b715cd8fe3dc6db7
Rafael Alberto <rafael.alberto1703@gmail.com>
2026-01-30 20:13:15 -0300

    Starting transaction flow E2E test

---
+++ src/test/java/io/rafaalberto/transactionstreamprocessor/integration/application/TransactionFlowE2ETest.java
@@ -0,0 +1,53 @@
+package io.rafaalberto.transactionstreamprocessor.integration.application;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import io.rafaalberto.transactionstreamprocessor.domain.transaction.Currency;
+import io.rafaalberto.transactionstreamprocessor.domain.transaction.TransactionType;
+import io.rafaalberto.transactionstreamprocessor.infrastructure.http.request.CreateTransactionRequest;
+import io.rafaalberto.transactionstreamprocessor.integration.config.KafkaInitializer;
+import io.rafaalberto.transactionstreamprocessor.integration.config.PostgresInitializer;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.client.RestClient;
+
+@ActiveProfiles({"test", "kafka"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = {PostgresInitializer.class, KafkaInitializer.class})
+public class TransactionFlowE2ETest {
+
+  private RestClient restClient;
+
+  @LocalServerPort private int port;
+
+  @BeforeEach
+  void setup() {
+    this.restClient = RestClient.builder().baseUrl("http://localhost:" + port).build();
+  }
+
+  @Test
+  void shouldCreateProcessAndPublishTransaction() {
+    var request =
+        new CreateTransactionRequest(
+            BigDecimal.ONE,
+            Currency.BRL,
+            TransactionType.CREDIT,
+            Instant.parse("2025-03-23T11:00:00Z"),
+            "external-reference-" + UUID.randomUUID());
+
+    var httpResponse =
+        restClient.post().uri("/transactions").body(request).retrieve().toBodilessEntity();
+
+    assertThat(httpResponse.getStatusCode())
+        .as("Transaction should be created successfully")
+        .isEqualTo(HttpStatus.CREATED);
+  }
+}
