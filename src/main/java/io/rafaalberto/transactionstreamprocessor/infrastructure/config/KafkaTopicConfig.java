bd1e5378f2df5edbee93feb80013a5861d51c24a
Rafael Alberto <rafael.alberto1703@gmail.com>
2026-02-20 20:42:39 -0300

    fix errors

--- src/main/java/io/rafaalberto/transactionstreamprocessor/infrastructure/config/KafkaTopicConfig.java
+++ src/main/java/io/rafaalberto/transactionstreamprocessor/infrastructure/config/KafkaTopicConfig.java
@@ -3,8 +3,10 @@ package io.rafaalberto.transactionstreamprocessor.infrastructure.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
+@Profile({"local", "test"})
 @Configuration
 public class KafkaTopicConfig {
 
